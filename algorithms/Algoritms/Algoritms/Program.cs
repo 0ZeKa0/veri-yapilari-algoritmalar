c81c1f0b3f4403ed9ffd1dd0ddf5f320c2952375
zehra <zehrakarakayabm@gmail.com>
2023-10-29 07:01:57 +0300

    struct example

---
+++ algorithms/Algoritms/Algoritms/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace Algoritms
+{
+    public struct Ogretmen
+    {
+        public string Ad;
+        public string Soyad;
+        public int Yas;
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogretmen ogretmen = new Ogretmen();
+
+        }
+    }
+
+
+
+}
