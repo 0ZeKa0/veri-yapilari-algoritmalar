ec936e9e5979ae71c914cf5892d98e51b44eb72f
zehra <zehrakarakayabm@gmail.com>
2023-11-23 17:15:40 +0300

    two sum problem solution

---
+++ algorithms/leetcode problems solutions/leetcode/leetcode/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+class GFG
+{
+
+    // Main Method
+    static public void Main()
+    {
+        int[]arr= { 2,7,8,1};
+        int target = 9;
+        Console.WriteLine(sum(arr,target));
+    }
+    // istenilen toplam için dizi içinden sayılar bulup toplama işlemi
+    // bu fonksiyon yalnızca 1 çift gönderir. 
+    // O(n^2) dir. 
+
+    static string  sum(int[] arr,int target)
+    {
+        int result = 0;
+        int[,] indices = new int[1, 2];
+        for(int i = 0;i<arr.Length;i++)
+        {
+            indices[0, 0] = i; //ilk indise arrdaydeki değer atandı. Ama değer değil indis isteniyor. Bu sebeple direkt indis atamasıyaptım.
+            result = target - arr[i];
+            for(int j = 0; j < arr.Length; j++)
+            {
+                if (arr[j] == result)
+                {
+                    indices[0, 1] = j;
+                    return indices[0, 0]+ " " + indices[0,1];
+                }
+
+            }
+        }
+        return " ";
+    }
+}
