c2864409f74782af35e3ef1149324ae111a41fc3
zehra <zehrakarakayabm@gmail.com>
2023-12-12 18:39:13 +0300

    binary search yapıldı.

---
+++ algorithms/binary search/BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,14 @@
+﻿using BinarySearch;
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            src binarysearch = new src(new int[] { 75, 85, 90 },3);
+
+        }
+    }
+}
---
+++ algorithms/binary search/BinarySearch/BinarySearch/src.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    public class src
+    {
+        public int[] dizi= new int[10];
+        public int target;
+     
+        public src(int[] sayilar,int target)
+        {
+            this.target = target;
+            this.dizi = sayilar;
+            Search(dizi, target);
+        }
+
+        public void Search(int[] dizi,int target)
+        {
+            Console.WriteLine(PrivateSearch(dizi,target));
+        }
+        private int PrivateSearch(int[] dizi, int target) {
+
+            int left = 0;
+            int right = dizi.Length - 1;
+            while (left <= right)
+            {
+                int mid= (left+right)/2;    
+                if (dizi[mid]==target)
+                {
+                    return mid;
+                }
+                else if (target <= dizi[mid])
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
