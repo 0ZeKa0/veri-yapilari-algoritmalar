81808ac63ab54bc5340b4826b34a3542d5549e2c
zehra <zehrakarakayabm@gmail.com>
2023-12-30 20:02:02 +0300

    stack tamamlandı

---
+++ lineerveriyapıları/Stack/Stack/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+Console.WriteLine("Hello, World!");
+
+Stack stack = new Stack();
+
+stack.Push(4);
+stack.Push(5);
+stack.Push(6);
+stack.Push(7);
+stack.Push(8);
+stack.Pop();
+Console.WriteLine(stack.Peek());
