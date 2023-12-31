540c1b36e3b795e7f613776fb3cce51d42590540
zehra <zehrakarakayabm@gmail.com>
2024-01-01 00:14:55 +0300

    avl trees right rotate and left rotate

---
+++ lineerolmayanveriyapıları/avltrees/avltrees/AVLNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avltrees
+{
+    public class AVLNode
+    {
+        public int Data;
+        public int Height;
+        public AVLNode Left, Right;
+        public AVLNode(int data) {
+            Data = data;
+            Height = 1;
+            Left = Right = null;
+         
+        }
+    }
+}
---
+++ lineerolmayanveriyapıları/avltrees/avltrees/AVLTrees.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avltrees
+{
+    public class AVLTrees
+    {
+        private  AVLNode root;
+
+        public AVLTrees() {
+            root = null;
+        }
+
+        private int Height( AVLNode node )
+        {
+            if ( node == null )
+            
+                return 0;
+    
+            return node.Height;
+        }
+
+        private int GetBalanced( AVLNode node )
+        {
+            if( node == null ) return 0;
+            return Height(node.Left)-Height(node.Right);
+        }
+
+        private AVLNode RightRotation( AVLNode node )
+        {
+            AVLNode x= node.Left;
+            AVLNode T1=x.Right;
+
+            x.Right = node;
+            node.Left = T1;
+
+            node.Height = Math.Max(Height(node.Left),Height(node.Right))+1;
+            x.Height = Math.Max(Height(x.Left),Height(x.Right)) + 1;
+
+            return node;
+        }
+
+        private AVLNode LeftRotation( AVLNode node ) {
+            AVLNode x = node.Right;
+            AVLNode T1 = x.Left;
+
+            x.Left = node;
+            node.Right = T1;
+
+            node.Height=Math.Max(Height(node.Right),Height(node.Left)) + 1;
+            x.Height = Math.Max(Height(x.Left), Height(x.Right)) + 1;
+
+            return node;
+        }
+
+    }
+}
