ee4b6d67fe672d1bfca70805b2c93e5c23d9cd10
zehra <zehrakarakayabm@gmail.com>
2024-01-02 19:29:01 +0300

    dictionary

--- lineerolmayanveriyapıları/avltrees/avltrees/AVLTrees.cs
+++ lineerolmayanveriyapıları/avltrees/avltrees/AVLTrees.cs
@@ -56,5 +56,6 @@ namespace avltrees
             return node;
         }
 
+
     }
 }
---
+++ lineerveriyapıları/Dictionary/Dictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+Console.WriteLine("Hello, World!");
+
+Dictionary<int, string> list = new Dictionary<int,string>();
+
+list.Add(1,"something");
+list.Add(2,"something");
+
+foreach (var item in list)
+{
+    if (item.Key == 2)
+    {
+        Console.WriteLine(item.Value);
+    }
+    else
+    {
+        Console.WriteLine(item.Key);
+    }
+}
